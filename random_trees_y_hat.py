438e31f83d310ac75a45fed1987921d338416bab
Shawn Oakley <shawnoakley@gmail.com>
2014-01-25 20:09:08 -0500

    Adds mrandom trees py script.

---
+++ random_trees_y_hat.py
@@ -0,0 +1,28 @@
+# http://blog.yhathq.com/posts/random-forests-in-python.html
+
+# # Generating data as example
+# import numpy as np
+# x = np.random.uniform(1, 100, 100)
+# y = np.log(x) + np.random.normal(0, .3, 1000)
+
+
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd 
+import numpy as np
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['is_train'] = np.random.uniform(0,1, len(df)) <= .75
+df['species'] = pd.Factor(iris.target, iris.target_names)
+df.head()
+
+train, test = df[df['is_train']==True], df[df['is_train']==False]
+
+features = df.columns[:4]
+clf = RandomForestClassifier(n_jobs=2)
+y, _ = pd.factorize(train['species'])
+clf.fit(train[features], y)
+
+preds = iris.target_names[clf.predict(test[features])]
+pd.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
